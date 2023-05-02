8a5c1e74f65505e1f7fc6fa6eb4789789eb9fb4c
Daisie <dacekebzere@gmail.com>
2023-05-02 21:45:40 +0200

    customized for big bang theory theme

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,11 +1,14 @@
 import random
 
+print("Welcome to Sheldon Cooper's 'Rock, Paper, Scissors, Lizard, Spock' Game! 🪨📄✂️🦎🖖")
+print("Let's play!")
+
 def play_game():
 
     user_wins = 0
     computer_wins = 0
     try_both = 0
-    options = ["rock", "paper", "scissors"]
+    options = ["rock", "paper", "scissors", "lizard", "spock"]
     options[0]
 
     while True:
@@ -14,22 +17,58 @@ def play_game():
         if user_input not in options:
             continue
 
-        random_number = random.randint(0, 2)
-        # rock: 0, paper: 1, scissors: 2
+    # rock: 0, paper: 1, scissors: 2, lizard: 3, spock: 4
+        random_number = random.randint(0, 4)
         computer_pick = options[random_number]
-        print("Computer picked", computer_pick + ".")
+        print("Sheldon picked", computer_pick + ".")
 
+    # Rock crushes Scissors !
         if user_input == "rock" and computer_pick == "scissors":
             print("You won!")
             user_wins += 1
             try_both += 1
-
+    # Paper covers Rock !
         elif user_input == "paper" and computer_pick == "rock":
             print("You won!")
             user_wins += 1
             try_both += 1
-
+    # Scissors cuts Paper !
         elif user_input == "scissors" and computer_pick == "paper":
+            print("You won!")
+            user_wins += 1
+            try_both += 1
+     # Rock crushes Lizard !       
+        elif user_input == "rock" and computer_pick == "lizard":
+            print("You won!")
+            user_wins += 1
+            try_both += 1
+    # Lizard poisons Spock !     
+        elif user_input == "lizard" and computer_pick == "spock":
+            print("You won!")
+            user_wins += 1
+            try_both += 1
+    # Spock smashes Scissors !
+        elif user_input == "spock" and computer_pick == "scissors":
+            print("You won!")
+            user_wins += 1
+            try_both += 1
+    # Scissors decapitates Lizard !       
+        elif user_input == "scissors" and computer_pick == "lizard":
+            print("You won!")
+            user_wins += 1
+            try_both += 1
+    # Lizard eats Paper !
+        elif user_input == "lizard" and computer_pick == "paper":
+            print("You won!")
+            user_wins += 1
+            try_both += 1
+    # Paper disproves Spock !
+        elif user_input == "paper" and computer_pick == "spock":
+            print("You won!")
+            user_wins += 1
+            try_both += 1
+    # Spock vaporizes Rock !
+        elif user_input == "spock" and computer_pick == "rock":
             print("You won!")
             user_wins += 1
             try_both += 1
@@ -39,11 +78,11 @@ def play_game():
             computer_wins += 1
             try_both += 1
 
-        if try_both == 5:
+        if try_both == 7:
             break
 
     print("You won", user_wins, " times!")
-    print("Computer won", computer_wins, " times!")
+    print("Sheldon won", computer_wins, " times!")
 
 while True:
     play_game()
